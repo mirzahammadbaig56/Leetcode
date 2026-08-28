1ab28f68d9bf7b57ce55089362e1669c8793188b
mirzahammadbaig56 <mirzahammadbaig56@gmail.com>
2026-08-28 10:39:40 +0500

    Time: 0 ms (100%), Space: 8.5 MB (83.43%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int reverse = 0;
+        while(x != 0){
+            if(reverse > INT_MAX/10)
+                return 0;
+            if(reverse == INT_MAX/10 && x % 10 > 7)
+                return 0;
+            if(reverse < INT_MIN/10)
+                return 0;
+            if(reverse == INT_MIN/10 && x % 10 < -8)
+                return 0;
+
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+        return reverse;
+    }
+};
